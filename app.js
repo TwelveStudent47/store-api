494037df9acc25b290aae4f338879c9136f41f80
Kevin Laczkó <kevin.laczko@pomscloud.ie>
2026-02-15 09:16:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to the API" });
+});
+
+/* app.use(notFoundMiddleware);
+app.use(errorMiddleware); */
+
+const start = async () => {
+    try {
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 5000}`);
+        });
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+start();
