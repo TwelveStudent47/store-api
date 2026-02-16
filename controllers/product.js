54c780e4edcc2b296a4c6390e4dbf1f2192b99d1
Kevin Laczkó <kevin.laczko@pomscloud.ie>
2026-02-16 16:37:49 +0100

    feat: enhance product retrieval to support featured query parameter

--- controllers/product.js
+++ controllers/product.js
@@ -6,7 +6,14 @@ exports.getApiHealth = (req, res) => {
 
 exports.getAllProducts = async (req, res) => {
     try {
-        const products = await Product.find(req.query);
+        const { featured } = req.query;
+        const queryObject = {};
+
+        if (featured) {
+            queryObject.featured = featured === 'true' ? true : false;
+        }
+
+        const products = await Product.find(queryObject);
         res.status(200).json({ success: true, nbHits: products.length, data: products });
     } catch (err) {
         res.status(500).json({ error: 'Server error' });
