ed63964d6559c9e8dfa4c0e9b068669c2a2dcba1
Kevin Laczkó <kevin.laczko@pomscloud.ie>
2026-02-17 09:12:28 +0100

    feat: enhance product retrieval to support company and name query parameters

--- controllers/product.js
+++ controllers/product.js
@@ -6,16 +6,37 @@ exports.getApiHealth = (req, res) => {
 
 exports.getAllProducts = async (req, res) => {
     try {
-        const { featured } = req.query;
+        const { featured, company, name } = req.query;
         const queryObject = {};
 
         if (featured) {
             queryObject.featured = featured === 'true' ? true : false;
         }
 
+        if (company) {
+            queryObject.company = company;
+        }
+
+        if (name) {
+            queryObject.name = { $regex: name, $options: 'i' };
+        }
+
         const products = await Product.find(queryObject);
-        res.status(200).json({ success: true, nbHits: products.length, data: products });
+        
+        if (!products) {
+            return res.status(404).json({
+                success: false,
+                nbHits: 0,
+                error: 'No products found' 
+            });
+        }
+
+        res.status(200).json({ 
+            success: true, 
+            nbHits: products.length, 
+            data: products 
+        });
     } catch (err) {
-        res.status(500).json({ error: 'Server error' });
+        res.status(500).json({ error: err.message });
     }
 }
