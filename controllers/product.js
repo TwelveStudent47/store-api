f10a97b90b5258aacc2c44720c7288ec552217d5
Kevin Laczkó <kevin.laczko@pomscloud.ie>
2026-03-11 21:19:55 +0100

    feat: update product retrieval to support pagination with page and limit query parameters

--- controllers/product.js
+++ controllers/product.js
@@ -34,6 +34,12 @@ exports.getAllProducts = async (req, res) => {
             const fieldsList = fields.split(",").join(" ");
             result = result.select(fieldsList);
         }
+
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const skip = (page - 1) * limit
+
+        result = result.skip(skip).limit(limit);
         
         const products = await result;
 
