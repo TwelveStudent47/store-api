e500d437df1c480d962b4444c2d8cbd23592346c
Kevin Laczkó <kevin.laczko@pomscloud.ie>
2026-03-13 05:24:31 +0100

    feat: enhance product retrieval to support numeric filters for price and rating

--- controllers/product.js
+++ controllers/product.js
@@ -6,7 +6,7 @@ exports.getApiHealth = (req, res) => {
 
 exports.getAllProducts = async (req, res) => {
     try {
-        const { featured, company, name, sort, fields } = req.query;
+        const { featured, company, name, sort, fields, numericFilters } = req.query;
         const queryObject = {};
 
         if (featured) {
@@ -19,6 +19,27 @@ exports.getAllProducts = async (req, res) => {
 
         if (name) {
             queryObject.name = { $regex: name, $options: 'i' };
+        }
+
+        if (numericFilters) {
+            const operatorMap = {
+                '>': '$gt',
+                '>=': '$gte',
+                '=': '$eq',
+                '<': '$lt',
+                '<=': '$lte'
+            };
+
+            const regEx = /\b(>|>=|=|<|<=)\b/g;
+            let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
+
+            const options = ["price", "rating"];
+            filters = filters.split(",").forEach((item) => {
+                const [field, operator, value] = item.split("-");
+                if (options.includes(field)) {
+                    queryObject[field] = { [operator]:Number(value)}
+                }
+            });
         }
 
         let result = Product.find(queryObject);
